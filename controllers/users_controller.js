c1e02932d1342d44a2c22321998a72bd7052c30d
nirbhay <yadavnirbhay2@gmail.com>
2022-11-12 23:27:08 +0530

    cookies and sign up created

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,59 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res){
     return res.render('users',{
         username : "Nirbhay",
         title: "profile"
     })
+};
+
+// render signIn page 
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: 'SocailCode | Sign In'
+    });
+};
+
+// render signUp page 
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: 'SocailCode | Sign Un'
+    });
+};
+
+// get the sign up data 
+module.exports.create = function(req, res){
+    if(req.body.password != req.body.confirm_password)
+    {
+        return res.redirect('back');
+    }
+
+    // findOne function will find an email in our database collection with email req.body.email and if found that document will be repersent by user
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){
+            console.log('Error in finding user in signing up'); return;
+        }
+
+        if(!user)
+        {
+            User.create(req.body, function(err, user){
+                if(err)
+                {
+                    console.log('Error creating user while signing up'); return;
+                }
+
+                return res.redirect('/users/sign-in');
+            })
+        }
+        else
+        {
+            return res.redirect('back');
+        }
+    });
+
+}
+
+// sing in and create session 
+module.exports.createSession = function(req, res){
+    // todo Later 
 }
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const { request } = require('express');
 const express = require('express');
+// install cookie-parser module through npm to use cookie in our webpage
+const cookieParser = require('cookie-parser');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 8000;
 const db = require('./config/mongoose');
 
 // import express-ejs-layouts module before processing the request made by browser since we have to render the layout page in response to that request 
 const expressLayouts = require('express-ejs-layouts');
+
+// use middleware to setup cookieParser 
+app.use(cookieParser());
 
 // below line will set the expressjs to look for any static at below mentioned location like css js images and etc
 app.use(express.static('./assets'));
@@ -13,6 +19,9 @@ app.use(express.static('./assets'));
 // extract styles and scripts from sub pages into the layout 
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
+
+// middleware used to decode the encrypted data with the help of bodyparser 
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(expressLayouts);
 
