eaa3734e1bc921cb504c1339215a6ffc0730e43c
nirbhay <yadavnirbhay2@gmail.com>
2022-12-04 00:48:32 +0530

    database for comments has been successfully setup

---
+++ controllers/comments_controller.js
@@ -0,0 +1,29 @@
+const Comment = require('../models/comment');
+const Post = require('../models/post');
+
+module.exports.create = function(req, res){
+    //we are checking first if post is already present in our database or not as use can fiddle our code by inspecting the html comment form and then changing the post._id
+    Post.findById(req.body.post, function(err,post){
+        if(err){
+            console.log("error in finding post of the comment");
+            return;
+        }
+        if(post){
+            Comment.create({
+                content:req.body.content,
+                post: req.body.post,
+                user: req.user._id
+            }, function(err, comment){
+                if(err){
+                    console.log('Error in finding comment');
+                    return;
+                }
+                
+                post.comments.push(comment); //push is a command of mongodb
+                post.save(); //save tells the database that this is the final result save it in the database(before saving that data, that data is just present in memory).
+
+                res.redirect('/');
+            });
+        }
+    });
+}
