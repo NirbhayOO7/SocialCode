ae0d6c4970bb9744fb1025e2c2186df61ff222c2
nirbhay <yadavnirbhay2@gmail.com>
2023-01-27 02:09:24 +0530

    nodemailer is being added to sent mails to user using mailing services

---
+++ config/nodemailer.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const path = require('path');
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    // if true the connection will use TLS when connecting to server. If false (the default) then TLS is used if server supports the STARTTLS extension. In most cases set this value to true if you are connecting to port 465. For port 587 or 25 keep it false
+    auth: {
+        user: "uchihanirbhay02@gmail.com",
+        pass: "05296202A!e"
+    }
+});
+
+let renderTemplate = (data, relativePath) =>{
+    let mailHTML;
+    ejs.renderFile(path.join(__dirname, '../views/mailers', relativePath)) // relativePath is the location from where this function is being called.
+    data,
+    function(err, template){
+        if(err){console.log('error in rendering template'); return;};
+
+        mailHTML = template;
+    }
+
+    return mailHTML;
+}
+
+module.exports = {
+    transporter: transporter,
+    renderTemplate: renderTemplate
+}
